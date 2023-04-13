a69f6acc7743d1352adcf79bcd60b95638ac35ff
Miguel-Aparicio <miguel.apgd@gmail.com>
2023-04-13 11:21:16 +0200

    Archivos modificados y readme

--- ejercicios/ejercicio5.py
+++ ejercicios/ejercicio5.py
@@ -1,10 +1,11 @@
-#Algoritmo cadenainvertida
-# Entrada
-cadena = input("Introduzca su cadena de caracteres para ser invertida: ")
-cadena_invertida = []
-# Proceso
-for i in range((len(cadena)-1),-1,-1):
-    cadena_invertida.append(cadena[i])
-cadena_invertida = "".join(cadena_invertida)
-# Salida
-print(cadena_invertida)
+def principal():
+    #Algoritmo cadenainvertida
+    # Entrada
+    cadena = input("Introduzca su cadena de caracteres para ser invertida: ")
+    cadena_invertida = []
+    # Proceso
+    for i in range((len(cadena)-1),-1,-1):
+        cadena_invertida.append(cadena[i])
+    cadena_invertida = "".join(cadena_invertida)
+    # Salida
+    print(cadena_invertida)
